b89e4666d6174ff7e9111d691ab19d931ac2bb38
Mudassir710 <mudassir.anas@gmail.com>
2020-08-07 23:18:06 +0530

    MD

---
+++ GFG/Arrays/Minimum-Platforms.cpp
@@ -0,0 +1,52 @@
+/*
+Mohammed Mudassir Mohiuddin
+
+Given arrival and departure times of all trains that reach a railway station. Your task is to find the minimum number of platforms required for the railway station so that no train waits.
+
+Note: Consider that all the trains arrive on the same day and leave on the same day. Also, arrival and departure times will not be same for a train, but we can have arrival time of one train equal to departure of the other. In such cases, we need different platforms, i.e at any given instance of time, same platform can not be used for both departure of a train and arrival of another.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int num;
+	cin>>num;
+	int arr[num],dep[num];
+	for(int i=0;i<num;i++)
+	  cin>>arr[i];
+	for(int i=0;i<num;i++)
+	  cin>>dep[i];
+	sort(arr, arr+num);
+	sort(dep, dep+num);
+	int res=0,plat=1;
+	
+	int i=1, j=0;
+	
+	while(i<num && j<num)
+	{
+	    if(arr[i] <= dep[j])
+	    {
+	        ++plat;
+	        i++;
+	    }
+	    else
+	    {
+	        --plat;
+	        j++;
+	    }
+	    if(plat > res) res = plat;
+	}
+	cout<<res<<"\n";
+	  
+}
+int main()
+ {
+	int tests;
+	cin>>tests;
+	while(tests--)
+	{
+	    solve();
+	}
+	return 0;
+}
