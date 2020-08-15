1aced09a5e73e4114a2bd079643f6bf0472585dc
Mudassir710 <mudassir.anas@gmail.com>
2020-08-15 18:50:48 +0530

    MD

---
+++ GFG/LinkedList/Implement-Queue-using-Linked-List.cpp
@@ -0,0 +1,150 @@
+/*Mohammed Mudassir Mohiuddin*/ 
+/*
+Implement a Queue using Linked List. 
+A Query Q is of 2 Types
+(i) 1 x   (a query of this type means  pushing 'x' into the queue)
+(ii) 2     (a query of this type means to pop an element from the queue and print the poped element)
+
+Example 1:
+
+Input:
+Q = 5
+Queries = 1 2 1 3 2 1 4 2
+Output: 2 3
+Explanation: n the first testcase
+1 2 the queue will be {2}
+1 3 the queue will be {2 3}
+2   poped element will be 2 the
+    queue will be {3}
+1 4 the queue will be {3 4}
+2   poped element will be 3.
+
+Example 2:
+
+Input:
+Q = 4
+Queries = 1 2 2 2 1 3 
+Output: 2 -1
+Explanation: In the second testcase 
+1 2 the queue will be {2}
+2   poped element will be {2} then
+    the queue will be empty. 
+2   the queue is empty and hence -1
+1 3 the queue will be {3}.
+Your Task:
+Complete the function push() which takes an integer as input parameter and pop() which will remove and return an element(-1 if queue is empty).
+
+Expected Time Complexity: O(1).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= Q <= 100
+1 <= x <= 100
+*/ 
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+};
+
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+// } Driver Code Ends
+
+
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+/* The method push to push element into the queue*/
+void MyQueue:: push(int x)
+{
+        // Your Code
+    struct QueueNode* temp = new  QueueNode(x);
+    if(rear == NULL)
+    {
+        front = rear = temp;
+        return;
+    }
+    rear->next = temp;
+    rear = temp;
+}
+
+/*The method pop which return the element
+  poped out of the queue*/
+int MyQueue :: pop()
+{
+    if(front == NULL)
+        return -1;
+    
+    int tp = front->data;
+    struct QueueNode* temp = front;
+    front = front->next;
+    if (front == NULL) 
+        rear = NULL; 
+    temp->next = NULL;
+    delete(temp);
+    return tp;
+}
+
