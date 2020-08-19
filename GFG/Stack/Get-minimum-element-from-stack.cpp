f9988851492c7bb1ebd74c1b04af33adfd08d5a1
Mudassir710 <mudassir.anas@gmail.com>
2020-08-19 22:47:51 +0530

    MD

---
+++ GFG/Stack/Get-minimum-element-from-stack.cpp
@@ -0,0 +1,142 @@
+/*Mohammed Mudassir Mohiuddin*/ 
+/*
+Get minimum element from stack 
+You are given N elements and your task is to Implement a Stack in which you can get minimum element in O(1) time.
+
+Example 1:
+
+Input:
+push(2)
+push(3)
+pop()
+getMin()
+push(1)
+getMin()
+Output: 3 2 1
+Explanation: In the first test case for
+query 
+push(2)  the stack will be {2}
+push(3)  the stack will be {2 3}
+pop()    poped element will be 3 the
+         stack will be {2}
+getMin() min element will be 2 
+push(1)  the stack will be {2 1}
+getMin() min element will be 1
+Your Task:
+You are required to complete the three methods push() which take one argument an integer 'x' to be pushed into the stack, pop() which returns a integer poped out from the stack and getMin() which returns the min element from the stack. (-1 will be returned if for pop() and getMin() the stack is empty.)
+
+Expected Time Complexity : O(1) for all the 3 methods.
+Expected Auixilliary Space : O(1) for all the 3 methods.
+
+Constraints:
+1 <= Number of queries <= 100
+1 <= values of the stack <= 100
+*/ 
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+class _stack{
+stack<int> s;
+int minEle;
+public :
+    int getMin();
+    int pop();
+    void push(int);
+};
+
+
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int q;
+       cin>>q;
+        _stack *a = new _stack();
+       while(q--){
+
+       int qt;
+       cin>>qt;
+
+       if(qt==1)
+       {
+           //push
+           int att;
+           cin>>att;
+           a->push(att);
+       }
+       else if(qt==2)
+       {
+           //pop
+           cout<<a->pop()<<" ";
+       }
+       else if(qt==3)
+       {
+           //getMin
+           cout<<a->getMin()<<" ";
+       }
+       }
+       cout<<endl;
+   }
+
+}
+
+
+// } Driver Code Ends
+
+
+/*
+The structure of the class is as follows
+class _stack{
+stack<int> s;
+int minEle;
+public :
+    int getMin();
+    int pop();
+    void push(int);
+};
+*/
+
+/*returns min element from stack*/
+int _stack :: getMin()
+{
+    if (s.empty()) 
+        return -1;     
+    return minEle;
+}
+
+/*returns poped element from stack*/
+int _stack ::pop()
+{
+   //Your code here
+    if(s.empty())
+        return -1;
+    int t = s.top();
+    s.pop();
+    if(t < minEle)
+    {
+        int temp = minEle;
+        minEle = 2 * minEle - t;
+        return temp;
+    }
+    return t;
+}
+
+/*push element x into the stack*/
+void _stack::push(int x)
+{
+    if(s.empty())
+    {
+        s.push(x);
+        minEle = x;
+        return;
+    }
+    if(x >= minEle)
+        s.push(x);
+    else
+    {
+        s.push(2*x - minEle);
+        minEle = x;
+    }
+}
