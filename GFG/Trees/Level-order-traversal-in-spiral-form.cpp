546ee37d49b6a342aff9036086a63c8eeba5115f
Mudassir710 <mudassir.anas@gmail.com>
2020-08-27 22:43:46 +0530

    MD

---
+++ GFG/Trees/Level-order-traversal-in-spiral-form.cpp
@@ -0,0 +1,190 @@
+/*Mohammed Mudassir Mohiuddin*/ 
+/*
+Complete the function to print spiral order traversal of a tree. For below tree, function should print 1, 2, 3, 4, 5, 6, 7.
+
+
+ 
+ 
+
+Example 1:
+
+Input:
+      1
+    /   \
+   3     2
+Output:1 3 2
+
+Example 2:
+
+Input:
+           10
+         /     \
+        20     30
+      /    \
+    40     60
+Output: 10 20 30 60 40 
+Your Task:
+The task is to complete the function printSpiral() which prints the elements in spiral form of level order traversal. The newline is automatically appended by the driver code.
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+0 <= Number of nodes <= 105
+1 <= Data of a node <= 105
+*/ 
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+void printSpiral(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+int main() {
+    int t;
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+
+        printSpiral(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
+
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+void printSpiral(Node *root)
+{
+   if(!root) return;
+   stack<Node*> LRstack;
+   stack<Node*> RLstack;
+   RLstack.push(root);
+   while(!LRstack.empty() || !RLstack.empty())
+   {
+       while(!LRstack.empty())
+       {
+           Node* temp = LRstack.top();
+           LRstack.pop();
+           cout<<temp->data <<" ";
+            if(temp->left)
+                RLstack.push(temp->left);
+            if(temp->right)
+                RLstack.push(temp->right);    
+       }
+       while(!RLstack.empty())
+       {
+           Node* temp = RLstack.top();
+           RLstack.pop();
+           cout<<temp->data<<" ";
+           if(temp->right)
+                LRstack.push(temp->right);
+            if(temp->left)
+                LRstack.push(temp->left);
+       }
+   }
+    
+}
+
