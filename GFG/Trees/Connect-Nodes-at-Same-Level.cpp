447db469c674f393ebf4bb05f9214f4fdb970d88
Mudassir710 <mudassir.anas@gmail.com>
2020-09-01 19:08:48 +0530

    MD

---
+++ GFG/Trees/Connect-Nodes-at-Same-Level.cpp
@@ -0,0 +1,222 @@
+/*Mohammed Mudassir Mohiuddin*/ 
+/*
+Given a binary tree, connect the nodes that are at same level. You'll be given an addition nextRight pointer for the same.
+
+Initially, all the nextRight pointers point to garbage values. Your function should set these pointers to point next right for each node.
+       10                       10 ------> NULL
+      / \                       /      \
+     3   5       =>     3 ------> 5 --------> NULL
+    / \     \               /  \           \
+   4   1   2          4 --> 1 -----> 2 -------> NULL
+
+ 
+
+Example 1:
+
+Input:
+     3
+   /  \
+  1    2
+Output:
+3 1 2
+1 3 2
+Explanation:The connected tree is
+        3 ------> NULL
+     /    \
+    1-----> 2 ------ NULL
+Example 2:
+
+Input:
+      10
+    /   \
+   20   30
+  /  \
+ 40  60
+Output:
+10 20 30 40 60
+40 20 60 10 30
+Explanation:The connected tree is
+         10 ----------> NULL
+       /     \
+     20 ------> 30 -------> NULL
+  /    \
+ 40 ----> 60 ----------> NULL
+*/ 
+
+
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node* nextRight;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->nextRight = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+void connect(struct Node *p);
+
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+
+
+void printSpecial(Node *root)
+{
+   if (root == NULL)
+     return;
+
+   Node* next_root=NULL;
+
+   while (root != NULL)
+   {
+      cout<< root->data<<" ";
+
+      if( root->left && (!next_root) )
+        next_root = root->left;
+      else if( root->right && (!next_root)  )
+        next_root = root->right;
+
+      root = root->nextRight;
+   }
+   
+   printSpecial(next_root);
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+       return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d\n", &t);
+  while (t--)
+  {
+     string s;
+     getline(cin, s);
+     Node* root = buildTree(s);
+
+     connect(root);
+     printSpecial(root);
+     cout<<endl;
+     inorder(root);
+     cout<<endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
+
+
+/* struct Node
+{
+  int data;
+  Node *left,  *right;
+  Node *nextRight;  // This has garbage value in input trees
+}; */
+void GetConnections(Node* root, int h, unordered_map<int, vector<Node*>> &map)
+{
+    if(!root) return;
+    map[h].push_back(root);
+    GetConnections(root->left, h+1, map);
+    GetConnections(root->right, h+1, map);
+}
+// Should set the nextRight for all nodes
+void connect(Node *p)
+{
+   unordered_map<int, vector<Node*>> map;
+   
+   GetConnections(p, 0, map);
+   
+   for(auto itr : map)
+   {
+       int i;
+       for(i=0; i<itr.second.size()-1; ++i)
+       {
+            itr.second[i]->nextRight = itr.second[i+1];
+       }
+   }
+}
+
+
